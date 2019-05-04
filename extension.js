e31f702e715389da789f97a020aa56b93b32d69c
xw <daysme@gmail.com>
2019-05-04 15:40:55 +0800

    feat: 自动提示功能， 简单示例

--- extension.js
+++ extension.js
@@ -28,7 +28,7 @@ const warn = (msg = '没有得到任何代码') => vscode.window.showWarningMess
 function activate(context) {
   let runSelection = vscode.commands.registerCommand('aar.runSelection', function () {
     // 运行所选内容
-    let editor =vscode.window.activeTextEditor
+    let editor = vscode.window.activeTextEditor
     if (editor) {
       let selection = editor.selection;
       let code = editor.document.getText(selection);
@@ -39,7 +39,6 @@ function activate(context) {
         warn()
       }
     }
-    context.subscriptions.push(runSelection);
 
   });
 
@@ -57,115 +56,68 @@ function activate(context) {
         warn()
       }
     }
-    context.subscriptions.push(runAll);
 
   });
 
 
   // 示例: https://github.com/Microsoft/vscode-extension-samples/blob/master/completions-sample/src/extension.ts
   // https://www.kancloud.cn/shangyewangchuan/vs_code/972973
-  vscode.languages.registerCompletionItemProvider('aar', { // languages.id
+  // 通过 CompletionItemProvider 实现自动完成
+  let provider = vscode.languages.registerCompletionItemProvider('aar', { // languages.id
     provideCompletionItems: (document, position) => {
+      // 普通的自动补全
+      const simpleCompletion = new vscode.CompletionItem('this');
+      // 自定义插入文本和文档的补全
+      const snippetCompletion = new vscode.CompletionItem('rget');
+      // 插入文档
+      snippetCompletion.insertText = new vscode.SnippetString('rget(${1:/*函数或起始位置*/},${2})');
+      // 提示的文档
+      snippetCompletion.documentation = new vscode.MarkdownString('参数@2通常使用一个函数调用并可能返回多个值,\n参数@1指定截取返回值的起始位置,可使用负数表示尾部倒计数');
+      const commitCharacterCompletion = new vscode.CompletionItem('console');
+      // 使用提交字符确定输入
+      commitCharacterCompletion.commitCharacters = ['.'];
+      commitCharacterCompletion.documentation = new vscode.MarkdownString('按 `.` 确认输入 `console.`');
+      const commandCompletion = new vscode.CompletionItem('new');
+      // 选择使用的图标
+      commandCompletion.kind = vscode.CompletionItemKind.Method;
+      commandCompletion.insertText = 'new ';
+      // 重新触发自动完成
+      commandCompletion.command = { command: 'editor.action.triggerSuggest', title: '重新触发自动完成...' };
+      // 以数组形式返回所有自动完成项
       return [
-        {
-          label: 'eq(, )',
-          detail: 'eq(, )',
-          documentation: new vscode.MarkdownString('比较两个对象类型是否相同'),
-          insertText: new vscode.SnippetString('eq(${1}, ${2})'),
-        },
-        {
-          label: 'rget()',
-          detail: 'rget()',
-          documentation: new vscode.MarkdownString('调用type函数检测对象类型\n并返回最后一个类型描述'),
-          insertText: new vscode.SnippetString('rget(${1})'),
-        },
-        {
-          label: 'null',
-          detail: 'null',
-          documentation: new vscode.MarkdownString('空值：基本数据类型'),
-          insertText: new vscode.SnippetString('null'),
-        },
-        {
-          label: 'pointer',
-          detail: 'pointer',
-          documentation: new vscode.MarkdownString('指针值：基本数据类型'),
-          insertText: new vscode.SnippetString('pointer'),
-        },
-        {
-          label: 'boolean',
-          detail: 'boolean',
-          documentation: new vscode.MarkdownString('布尔值：基本数据类型'),
-          insertText: new vscode.SnippetString('boolean'),
-        },
-        {
-          label: 'string',
-          detail: 'string',
-          documentation: new vscode.MarkdownString('字符串：基本数据类型 '),
-          insertText: new vscode.SnippetString('string'),
-        },
-        {
-          label: 'number',
-          detail: 'number',
-          documentation: new vscode.MarkdownString('数值：基本数据类型'),
-          insertText: new vscode.SnippetString('number'),
-        },
-        {
-          label: 'table',
-          detail: 'table',
-          documentation: new vscode.MarkdownString('表：基本数据类型'),
-          insertText: new vscode.SnippetString('table'),
-        },
-        {
-          label: 'buffer',
-          detail: 'buffer',
-          documentation: new vscode.MarkdownString('raw.buffer函数创建的缓冲区指针\n可作为字节数组使用,可传入API作为指针参数'),
-          insertText: new vscode.SnippetString('buffer'),
-        },
-        {
-          label: 'cdata',
-          detail: 'cdata',
-          documentation: new vscode.MarkdownString('内核对象：基本数据类型  '),
-          insertText: new vscode.SnippetString('cdata'),
-        },
-        {
-          label: 'fiber',
-          detail: 'fiber',
-          documentation: new vscode.MarkdownString('纤程：基本数据类型'),
-          insertText: new vscode.SnippetString('fiber'),
-        },
-        {
-          label: 'function',
-          detail: 'function',
-          documentation: new vscode.MarkdownString('函数：基本数据类型 '),
-          insertText: new vscode.SnippetString('function'),
-        },
-        {
-          label: 'class',
-          detail: 'class',
-          documentation: new vscode.MarkdownString('类：基本数据类型 '),
-          insertText: new vscode.SnippetString('class'),
-        },
-        {
-          label: 'file',
-          detail: 'file',
-          documentation: new vscode.MarkdownString('扩展数据类型\n使用io.open打开的文件对象 '),
-          insertText: new vscode.SnippetString('file'),
-        },
-        {
-          label: 'isString()',
-          detail: 'isString()',
-          documentation: new vscode.MarkdownString('参数如果string或buffer类型返回true'),
-          insertText: new vscode.SnippetString('isString(${1})'),
-        },
-        {
-          label: '(.(需要返回数据类型的变量)',
-          detail: '(.(需要返回数据类型的变量)',
-          documentation: new vscode.MarkdownString('检测数据类型,返回值1为基本数据类型\n如果返回两个或三个值，最后的返回值为元表中_type的值,\n结构体返回三个值，第二个返回值为结构体声明'),
-          insertText: new vscode.SnippetString('(.(${1}需要返回数据类型的变量)'),
-        },
-      ]
+        simpleCompletion,
+        snippetCompletion,
+        commitCharacterCompletion,
+        commandCompletion
+      ];
+
     }
-  }, ['type.'])
+  })
+
+  // 自动提示 console 下的几个方法
+  const provider2 = vscode.languages.registerCompletionItemProvider('aar', {
+    provideCompletionItems(document, position) {
+      // position 返回了当前在第几行， 第几个字符位置
+      let linePrefix = document.lineAt(position).text.substr(0, position.character);
+      // 获取当前行开始到光标位置的文档， 检查尾部是否有 console. ， 如果有， 显示一些可供选择的方法
+      if (!linePrefix.endsWith('console.')) {
+        return undefined;
+      }
+      return [
+        new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
+        new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
+        new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
+      ];
+    }
+  }, ['.']) // 键入 . 时触发
+
+  // 注册命令会返回一个可清理的对象。将此对象添加到 插件上下文中的 subscriptions 列表中。以便在不需要时 vscode 可以清理此插件命令占用的资源
+  context.subscriptions.push(
+    runSelection,
+    runAll,
+    provider,
+    provider2,
+  );
 
 }
 exports.activate = activate;
